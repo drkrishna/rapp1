9f50638d8e989c9cce2a6eca1dd6a0a417cb8ca1
Krishna <krishnak.thakur2@gmail.com>
2023-03-05 11:25:01 +0530

    another commit from first session on react js

---
+++ src/childofapp.jsx
@@ -0,0 +1,9 @@
+import react from "react"
+
+function Child() {
+    return (
+        <p>i am child of App</p>
+    )
+}
+
+export default Child;
---
+++ src/hello.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+function Hello() {
+  return <p>new Hello</p>;
+}
+
+export default Hello;
